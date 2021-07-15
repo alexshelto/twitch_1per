6bea2017e0aa17d4cdd546c808a84855466f812d
alexshelto <alexandershelton@icloud.com>
2021-07-15 12:03:16 -0500

    dynamic page for user

--- src/main/routes.py
+++ src/main/routes.py
@@ -2,12 +2,12 @@
 from flask import (render_template, url_for, flash, redirect, request, abort, Blueprint)
 from src.main.forms import ChatForm
 
+from uuid import uuid1
+
 
 
 #Creating a blueprint instance
 main = Blueprint('main', __name__)
-
-
 
 
 
@@ -21,21 +21,37 @@ def home():
 def create_feed():
     form = ChatForm()
     if form.validate_on_submit():
-        #form.channel.data, etc 
-        flash('BAMMMM', 'success')
+
+        user_id = uuid1()
+        print(f'Gave user the unique uuid: {user_id}')
 
         channel = form.channel.data
         prefix = form.prefix.data
         print(f'Channel: {channel}, Prefix: {prefix}')
 
-        return redirect(url_for('main.home'))
+        args = ({'id': user_id, 'channel': channel, 'prefix': prefix})
+
+        print(args)
+
+
+        return redirect(url_for('main.view', u_id=user_id, channel=channel, prefix=prefix))
 
     return render_template('create_feed.html', title='Create Entry', form=form)
 
 
-@main.route('/view/<string:id>', methods=['GET', 'POST'])
-def view(id):
-    pass
+@main.route('/view/<string:u_id>/<string:channel>/<string:prefix>', methods=['GET', 'POST'])
+def view(u_id, channel, prefix):
+    print(f'''
+    uid: {u_id}
+    channel: {channel}
+    prefix: {prefix}
+    ''')
+
+    
+    ## Connect To Twitch IRC
+    ## Render chat messages
+    return '<h1> blah </h1>'
+
 
 
     
